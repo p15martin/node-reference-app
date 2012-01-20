031fd6fbd5a99f57a944e8559f43fa79b1a1888c
Peter Martin <peter@mailhop.co.uk>
2012-01-20 09:59:42 -0800

    added async package

--- com/experiencecraftsmanship/contacts/app/Application.js
+++ com/experiencecraftsmanship/contacts/app/Application.js
@@ -1,26 +1,14 @@
 define(
-    [ "database", "rpcServer" ],
-    function( database, rpcServer ) {
-        function connectToDatabase( mongoUri ) {
-            database.connectToDatabase( mongoUri, function( error ) {
-                if( error ) {
-                    throw new ApplicationError( "Failed to connect to database " + mongoUri );                
-                } else {
-                    console.log( "Successfully connected to database: " + mongoUri );
-                }
-            });
+    [ "async", "database", "rpcServer" ],
+    function( async, database, rpcServer ) {
+        function connectToDatabase( mongoUri, callback ) {
+            database.connectToDatabase( mongoUri, callback );
         }
 
-        function startRpcServer( rpcServerPort ) {
-            rpcServer.startServer( rpcServerPort, function( error ) {
-                if( error ) {
-                    throw new ApplicationError( "Failed to start RPC server on port " + rpcServerPort );                
-                } else {
-                    console.log( "Successfully started RPC server on port: " + rpcServerPort );
-                }
-            });
+        function startRpcServer( rpcServerPort, callback ) {
+            rpcServer.startServer( rpcServerPort, callback );
         }
-
+        /*
         function ApplicationError( message ) {
             Error.call( this );
             Error.captureStackTrace( this, this.constructor );
@@ -31,12 +19,17 @@ define(
         }
 
         ApplicationError.prototype.__proto__ = Error.prototype;
-
+        */
         return {
             start: function( mongoUri, rpcServerPort ) {
-                connectToDatabase( mongoUri );
 
-                startRpcServer( rpcServerPort );
+                async.parallel([
+                        function( callback ) { connectToDatabase( mongoUri, callback ) },
+                        function( callback ) { startRpcServer( rpcServerPort, callback ) }
+                    ], function( error, results ) {
+                        console.log( "***************** ERROR: " + error );
+                        console.log( "***************** RESULTS: " + results );
+                });
             }
         }
     }
