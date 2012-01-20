c5bee5ac5eadcbca15358ddafb92e876cb995d58
Peter Martin <peter@mailhop.co.uk>
2012-01-20 14:51:04 -0800

    expanding logging

--- com/experiencecraftsmanship/contacts/data/dao/ContactDao.js
+++ com/experiencecraftsmanship/contacts/data/dao/ContactDao.js
@@ -3,7 +3,7 @@ define(
     function( ContactModel ) {
         return {
             addContact: function( firstName, lastName, cellNumber, callback ) {
-                console.log( "Creating a new contact: firstName='%s', 'lastName='%s', cellNumber='%d'", firstName, lastName, cellNumber );
+                console.log( "Creating a new contact in the database: firstName='%s', 'lastName='%s', cellNumber='%d'", firstName, lastName, cellNumber );
 
                 var contact = ContactModel.newInstance();
                 contact.firstName = firstName;
@@ -19,7 +19,7 @@ define(
                     } else {
                         var id = result._id;
 
-                        console.log( "Successfully created the new contact (firstName='%s', 'lastName='%s', cellNumber='%d') in the databasewith id '%s'", firstName, lastName, cellNumber, id );
+                        console.log( "Successfully created the new contact (firstName='%s', 'lastName='%s', cellNumber='%d') in the database with id '%s'", firstName, lastName, cellNumber, id );
 
                         if ( callback ) {
                             callback( null, { _id: id } );
@@ -28,7 +28,7 @@ define(
                 });
             },
             findAllContactsByLastName: function( lastName, callback ) {
-                console.log( "Finding all contacts with lastName '%s'", lastName );
+                console.log( "Finding all contacts in the database with lastName '%s'", lastName );
 
                 ContactModel.find( { "lastName": lastName }, function ( error, result ) {
                     if ( error ) {
@@ -38,7 +38,7 @@ define(
                             callback( error );
                         }
                     } else {
-                        console.log( "Successfully found '%d' contacts with lastName '%s'", result.length, lastName );
+                        console.log( "Successfully found '%d' contacts in the database with lastName '%s'", result.length, lastName );
 
                         if ( callback ) {
                             callback( null, result );
@@ -47,17 +47,17 @@ define(
                 });
             },
             updateCellNumber: function( id, cellNumber, callback ) {
-                console.log( "Updating cell number '%s' for contact with id '%s'", cellNumber, id );
+                console.log( "Updating cell number '%s' for contact in database with id '%s'", cellNumber, id );
 
                 ContactModel.update( { "_id": id }, { 'cellNumber': cellNumber }, function( error, result ) {
                     if ( error ) {
-                        console.error( "Error updating cell number '%s' for contact with id '%s'", cellNumber, id );
+                        console.error( "Error updating cell number '%s' for contact in database with id '%s'", cellNumber, id );
 
                         if ( callback ) {
                             callback( error );
                         }
                     } else {
-                        console.log( "Successfully updated '%d' contact(s) with cell number '%s' and id '%s'", result, cellNumber, id );
+                        console.log( "Successfully updated '%d' contact(s) in the database with cell number '%s' and id '%s'", result, cellNumber, id );
 
                         if ( callback ) {
                             callback( null, { count: result } );
@@ -66,17 +66,17 @@ define(
                 });
             },
             deleteContact: function( id, callback ) {
-                console.log( "Deleting contact with id '%s'", id );
+                console.log( "Deleting contact from database with id '%s'", id );
 
                 ContactModel.remove( { "_id": id }, function( error, result ) {
                     if ( error ) {
-                        console.error( "Error deleting contact with id '%s'", id );
+                        console.error( "Error deleting contact from database with id '%s'", id );
 
                         if ( callback ) {
                             callback( error );
                         }
                     } else {
-                        console.log( "Successfully deleted '%d' contact(s) with id '%s'", result, id );
+                        console.log( "Successfully deleted '%d' contact(s) from the database with id '%s'", result, id );
 
                         if ( callback ) {
                             callback( null, { count: result } );
