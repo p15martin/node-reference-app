2f0464c84280e49fde3fa73b6699b5c45866ad4e
Peter Martin <peter@mailhop.co.uk>
2012-02-06 17:20:23 -0800

    refatoring for readability

--- test.sh
+++
@@ -1,4 +0,0 @@
-git commit -a -m "testing"
-git push heroku master
-heroku logs
-#curl -H "Content-Type: application/json" -d '{ "jsonrpc": "2.0", "method": "findAllContactsByLastName", "params": ["Bloggs"], "id":2 }' -i http://deep-fire-3746.herokuapp.com
